0a05ace0f7ecb063656d78abfd1ce0735e0080a4
DevDenzo <dyukselir@hotmail.co.uk>
2023-02-20 00:28:14 +0000

    setup

---
+++ bin/app.py
@@ -0,0 +1,13 @@
+__author__ = "Deniz Yukselir"
+__version__ = "0.0.1"
+__license__ = "Devite"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
