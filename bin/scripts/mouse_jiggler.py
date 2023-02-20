ae0a3f9f8378290e15121a6f16d70ee0dcf66781
DevDenzo <dyukselir@hotmail.co.uk>
2023-02-20 01:16:52 +0000

    mouse_jiggler script to boot the app added

---
+++ bin/scripts/mouse_jiggler.py
@@ -0,0 +1,24 @@
+import keyboard
+from scripts.enums.jiggler_mode import JigglerMode
+
+class MouseJiggler:
+    def __init__(self, mode, status):
+        self.mode = mode
+        self.status = status
+
+    def run():
+        MOUSE_JIGGLER_ACTIVATED = True
+
+        while MOUSE_JIGGLER_ACTIVATED:
+
+            if keyboard.read_key() == JigglerMode.RANDOM.value:
+                print("Random")
+                MOUSE_JIGGLER_ACTIVATED = False
+
+            elif keyboard.read_key() == JigglerMode.NATURAL.value:
+                print("Natural")
+                MOUSE_JIGGLER_ACTIVATED = False
+
+            elif keyboard.read_key() == JigglerMode.PATTERN.value:
+                print("Pattern")
+                MOUSE_JIGGLER_ACTIVATED = False
