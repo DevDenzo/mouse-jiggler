47648cf31482e69588c6252adcfa446c3af84983
DevDenzo <dyukselir@hotmail.co.uk>
2023-03-16 02:36:27 +0000

    refactor app

--- bin/app.py
+++ bin/app.py
@@ -5,13 +5,6 @@ __author__ = "Deniz Yukselir"
 __version__ = "0.0.1"
 __license__ = "Devite"
 
-
-# #Code to control keyboard
-# from pynput.keyboard import Key, Controller as KeyboardController
-# from pynput.mouse import Button, Controller as MouseController
-# keyboard = KeyboardController()
-# mouse = MouseController()
-
 def main():
     mouseSettings = MouseJigglerSettings()
     mousejiggler = MouseJigglerRunner(mouseSettings)
