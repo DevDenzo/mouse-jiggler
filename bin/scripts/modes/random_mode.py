43e33f04c4f9b1ce8035fd77f78dd51674ad93bf
DevDenzo <dyukselir@hotmail.co.uk>
2023-03-22 00:57:41 +0000

    Created 2 random modes, LINE and POINTS

--- bin/scripts/modes/random_mode.py
+++ bin/scripts/modes/random_mode.py
@@ -6,16 +6,30 @@ from scripts.helpers.coordinate_generator import CoordinateGenerator
 
 class RandomMode(Thread):
 
-    def __init__(self, screen_size_list, os, move_delay, status):
+    def __init__(self, screen_size_list, os, random_type, move_delay, status):
         Thread.__init__(self)
         self.screen_size_list = screen_size_list
         self.os = os
         self.move_delay = move_delay
         self.status = status
         self.mouse = Controller()
+        self.random_type = random_type
 
     def run(self):
-        random_coordinates = CoordinateGenerator().generate_random_coordinates_by_screensize(self.screen_size_list, 10000)
+        if self.random_type == "LINE":
+            random_generator = CoordinateGenerator()
+            random_points = random_generator.generate_random_coordinates_by_screensize(self.screen_size_list, 50)
+            random_coordinates = []
+
+            number_of_lines = 50
+
+            for n in range(1, number_of_lines):
+                if n == number_of_lines:
+                    random_coordinates = random_coordinates + random_generator.generate_coordinates_on_line(random_points[n], random_points[0], 500000)
+                
+                random_coordinates = random_coordinates + random_generator.generate_coordinates_on_line(random_points[n-1], random_points[n], 500000)
+        elif self.random_type == "POINTS":
+            random_coordinates = random_generator.generate_random_coordinates_by_screensize(self.screen_size_list, 300)
 
         while self.status == JigglerStatus.RUNNING:
             for cordinate in random_coordinates:
@@ -26,7 +40,7 @@ class RandomMode(Thread):
                     # If delay provided, check 4 times per second if status is still RUNNING or not
                     for n in range(0, self.move_delay*4):
                         if self.status == JigglerStatus.RUNNING:
-                            time.sleep(0.25)
+                            time.sleep(0.01)
                         else:
                             break
 
