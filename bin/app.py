b43c8d2b01c3afd8e0a0e257754ffede2842dca8
DevDenzo <dyukselir@hotmail.co.uk>
2023-04-10 00:42:54 +0100

    Updated author, version and contact information

--- bin/app.py
+++ bin/app.py
@@ -2,9 +2,9 @@ from scripts.mouse_jiggler_menu import MouseJigglerMenu
 from scripts.mouse_jiggler_runner import MouseJigglerRunner
 from scripts.settings.settings import MouseJigglerSettings
 
-__author__ = "Deniz Yukselir"
-__version__ = "0.0.1"
-__license__ = "Devite"
+__author__ = "Deniz Yukselir - @DevDenzo - dyukselir@hotmail.co.uk"
+__version__ = "1.0.1"
+__license__ = "Please request permission from Deniz Yukselir - @DevDenzo on Twitter"
 
 def main():
     mouseSettings = MouseJigglerSettings()
